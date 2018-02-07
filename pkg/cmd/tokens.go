46142f414ecd903286f91761b466174ee5d698d4
paul <petrukhin.paul@gmail.com>
2018-02-07 10:50:01 +0300

    remove check token ip parameter

--- pkg/cmd/tokens.go
+++ pkg/cmd/tokens.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"net"
-
 	"git.containerum.net/ch/json-types/auth"
 	user "git.containerum.net/ch/json-types/user-manager"
 )
@@ -13,10 +11,10 @@ const (
 	userAgent      = "kube-client"
 )
 
-// CheckToken -- consumes JWT token, user fingerprint and IP
+// CheckToken -- consumes JWT token, user fingerprint
 // If they're correct returns user access data:
 // list of namespaces and list of volumes OR uninitialized structure AND error
-func (client *Client) CheckToken(token, userFingerprint string, clientIP net.IP) (auth.CheckTokenResponse, error) {
+func (client *Client) CheckToken(token, userFingerprint string) (auth.CheckTokenResponse, error) {
 	resp, err := client.Request.
 		SetPathParams(map[string]string{
 			"access_token": token,
@@ -24,7 +22,6 @@ func (client *Client) CheckToken(token, userFingerprint string, clientIP net.IP)
 		SetResult(auth.CheckTokenResponse{}).
 		SetHeaders(map[string]string{
 			user.FingerprintHeader: userFingerprint,
-			user.ClientIPHeader:    clientIP.String(),
 			user.UserAgentHeader:   userAgent,
 		}).Get(client.serverURL + getCheckToken)
 	if err != nil {
