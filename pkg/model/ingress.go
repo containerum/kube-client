0dd7b173ed654bfcbc4d41ade4a8ac499b93317b
Ilia Donchenko <iliad@me.com>
2018-02-12 16:16:51 +0300

    Added createdat to ingress

--- pkg/model/ingress.go
+++ pkg/model/ingress.go
@@ -4,6 +4,7 @@ type Ingress struct {
 	Name      string `json:"name" binding:"required"`
 	TLSSecret string `json:"tls_secret,omitempty"`
 	Rule      Rule   `json:"rule" binding:"required"`
+	CreatedAt *int64 `json:"created_at,omitempty"`
 }
 
 type Rule struct {
