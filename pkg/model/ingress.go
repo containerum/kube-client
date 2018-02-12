4bfd03ad171fe54e57f04d198a0f9d24960649e3
Ilia Donchenko <iliad@me.com>
2018-02-12 12:57:53 +0300

    typo fix

--- pkg/model/ingress.go
+++ pkg/model/ingress.go
@@ -2,7 +2,7 @@ package model
 
 type Ingress struct {
 	Name      string `json:"name" binding:"required"`
-	TLSSecret string `json:"tls_secret, omitempty"`
+	TLSSecret string `json:"tls_secret,omitempty"`
 	Rule      Rule   `json:"rule" binding:"required"`
 }
 
@@ -14,5 +14,5 @@ type Rule struct {
 type Path struct {
 	Path        string `json:"path" binding:"required"`
 	ServiceName string `json:"service_name" binding:"required"`
-	ServicePort int    `json:"service_port, binding:"required"`
+	ServicePort int    `json:"service_port" binding:"required"`
 }
