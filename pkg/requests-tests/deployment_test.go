702fd8be9cefd47abaf4e2a5ca41fbcdc5746fc9
paul <petrukhin.paul@gmail.com>
2018-02-13 10:04:28 +0300

    add get deployment test

--- pkg/requests-tests/deployment_test.go
+++ pkg/requests-tests/deployment_test.go
@@ -3,6 +3,7 @@ package requests_test
 import (
 	"encoding/json"
 	"math/rand"
+	"reflect"
 	"testing"
 	"time"
 
@@ -19,20 +20,40 @@ var (
 )
 
 func TestDeployment(test *testing.T) {
-	client, err := cmd.CreateCmdClient(cmd.User{
-		Role: "admin",
-	})
+	client, err := cmd.CreateCmdClient(
+		cmd.ClientConfig{
+			ResourceAddr: "http://192.168.88.200:1213",
+			User: cmd.User{
+				Role: "admin",
+			},
+		})
 	if err != nil {
 		test.Fatalf("error while creating client: %v", err)
 	}
-	test.Run("deployment test", deploymentCreationTest(client))
+	fakeDeployment := newFakeDeployment(test)
+	test.Run("deployment creaton test",
+		deploymentCreationTest(client, testNamespace, fakeDeployment))
+	test.Run("get deployment test",
+		getDeploymentTest(client, testNamespace, fakeDeployment))
 }
 
-func deploymentCreationTest(client *cmd.Client) func(*testing.T) {
+func deploymentCreationTest(client *cmd.Client, namespace string, deployment model.Deployment) func(*testing.T) {
 	return func(test *testing.T) {
-		err := client.CreateDeployment(testNamespace, newFakeDeployment(test))
+		err := client.CreateDeployment(namespace, deployment)
 		if err != nil {
 			test.Fatalf("error while deployment creation: %v", err)
+		}
+	}
+}
+
+func getDeploymentTest(client *cmd.Client, namespace string, deployment model.Deployment) func(*testing.T) {
+	return func(test *testing.T) {
+		createdDeployment, err := client.GetDeployment(namespace, deployment.Name)
+		if err != nil {
+			test.Fatalf("error while getting deployment: %v", err)
+		}
+		if !reflect.DeepEqual(createdDeployment, deployment) {
+			test.Fatalf("created deployment doesn't match provided data!")
 		}
 	}
 }
