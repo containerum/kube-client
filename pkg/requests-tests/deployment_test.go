f93ef84049b532f7b79e43bc83cad6f5ec9246d3
paul <petrukhin.paul@gmail.com>
2018-02-14 12:32:43 +0300

    refactor test

--- pkg/requests-tests/deployment_test.go
+++ pkg/requests-tests/deployment_test.go
@@ -3,7 +3,6 @@ package requests_tests
 import (
 	"testing"
 
-	"git.containerum.net/ch/kube-client/pkg/cmd"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -14,50 +13,43 @@ const (
 )
 
 func TestDeployment(test *testing.T) {
-	client, err := cmd.CreateCmdClient(
-		cmd.ClientConfig{
-			ResourceAddr: "http://192.168.88.200:1213",
-			APIurl:       "http://192.168.88.200:1214",
-			User: cmd.User{
-				Role: "admin",
-			},
+	Convey("Test deployment methods", test, func() {
+		Convey("resource service methods", func() {
+			client := newResourceClient(test)
+			fakeResourceDeployment := newFakeResourceDeployment(test)
+			fakeUpdateImage := newFakeResourceUpdateImage(test)
+			Convey("create deployment", func() {
+				err := client.CreateDeployment(resourceTestNamespace, fakeResourceDeployment)
+				So(err, ShouldBeNil)
+			})
+			Convey("set container image", func() {
+				err := client.SetContainerImage(resourceTestNamespace,
+					fakeResourceDeployment.Name, fakeUpdateImage)
+				So(err, ShouldBeNil)
+			})
+			Convey("replace deployment", func() {
+				err := client.ReplaceDeployment(resourceTestNamespace, fakeResourceDeployment)
+				So(err, ShouldBeNil)
+			})
+			Convey("set replicas", func() {
+				err := client.SetReplicas(resourceTestNamespace, fakeResourceDeployment.Name, 4)
+				So(err, ShouldBeNil)
+			})
+			Convey("delete deployment", func() {
+				err := client.DeleteDeployment(resourceTestNamespace, fakeResourceDeployment.Name)
+				So(err, ShouldBeNil)
+			})
 		})
-	if err != nil {
-		test.Fatalf("error while creating client: %v", err)
-	}
-	Convey("Test resource service methods", test, func() {
-		fakeResourceDeployment := newFakeResourceDeployment(test)
-		fakeUpdateImage := newFakeResourceUpdateImage(test)
-		Convey("create deployment", func() {
-			err := client.CreateDeployment(resourceTestNamespace, fakeResourceDeployment)
-			So(err, ShouldBeNil)
-		})
-		Convey("set container image", func() {
-			err := client.SetContainerImage(resourceTestNamespace,
-				fakeResourceDeployment.Name, fakeUpdateImage)
-			So(err, ShouldBeNil)
-		})
-		Convey("replace deployment", func() {
-			err := client.ReplaceDeployment(resourceTestNamespace, fakeResourceDeployment)
-			So(err, ShouldBeNil)
-		})
-		Convey("set replicas", func() {
-			err := client.SetReplicas(resourceTestNamespace, fakeResourceDeployment.Name, 4)
-			So(err, ShouldBeNil)
-		})
-		Convey("delete deployment", func() {
-			err := client.DeleteDeployment(resourceTestNamespace, fakeResourceDeployment.Name)
-			So(err, ShouldBeNil)
-		})
-	})
-	Convey("Test KubeAPI methods", test, func() {
-		Convey("get deployment test", func() {
-			_, err := client.GetDeployment(kubeAPItestNamespace, kubeAPItestDeployment)
-			So(err, ShouldBeNil)
-		})
-		Convey("get deployment list", func() {
-			_, err := client.GetDeploymentList(kubeAPItestNamespace)
-			So(err, ShouldBeNil)
+		Convey("KubeAPI methods", func() {
+			client := newCubeAPIClient(test)
+			Convey("get deployment test", func() {
+				_, err := client.GetDeployment(kubeAPItestNamespace, kubeAPItestDeployment)
+				So(err, ShouldBeNil)
+			})
+			Convey("get deployment list", func() {
+				_, err := client.GetDeploymentList(kubeAPItestNamespace)
+				So(err, ShouldBeNil)
+			})
 		})
 	})
 }
