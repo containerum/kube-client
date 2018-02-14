cff283b1a339f69efc6d7f2bf258c6f0f9dfa7c9
paul <petrukhin.paul@gmail.com>
2018-02-14 10:21:46 +0300

    add get volume method test

---
+++ pkg/requests-tests/volume.go
@@ -0,0 +1,32 @@
+package requests_tests
+
+import (
+	"testing"
+
+	"git.containerum.net/ch/kube-client/pkg/cmd"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+const (
+	resourceTestVolume = "n1-volume"
+)
+
+func TestVolume(test *testing.T) {
+	client, err := cmd.CreateCmdClient(
+		cmd.ClientConfig{
+			ResourceAddr: "http://192.168.88.200:1213",
+			APIurl:       "http://192.168.88.200:1214",
+			User: cmd.User{
+				Role: "admin",
+			},
+		})
+	if err != nil {
+		test.Fatalf("error while creating client: %v", err)
+	}
+	Convey("Test resource API methods", test, func() {
+		Convey("get volume", func() {
+			_, err := client.GetVolume(resourceTestVolume, nil)
+			So(err, ShouldBeNil)
+		})
+	})
+}
