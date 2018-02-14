dce39c0beb3b410ecdda157df80d6d635fb69d1d
paul <petrukhin.paul@gmail.com>
2018-02-14 11:35:06 +0300

    refactor tests

--- pkg/requests-tests/volume_test.go
+++ pkg/requests-tests/volume_test.go
@@ -3,7 +3,6 @@ package requests_tests
 import (
 	"testing"
 
-	"git.containerum.net/ch/kube-client/pkg/cmd"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -12,23 +11,20 @@ const (
 )
 
 func TestVolume(test *testing.T) {
-	client, err := cmd.CreateCmdClient(
-		cmd.ClientConfig{
-			ResourceAddr: "http://192.168.88.200:1213",
-			APIurl:       "http://192.168.88.200:1214",
-			User: cmd.User{
-				Role: "admin",
-			},
-		})
-	if err != nil {
-		test.Fatalf("error while creating client: %v", err)
-	}
-	Convey("Test resource API methods", test, func() {
-		referenceVolume := newFakeResourceVolume(test)
-		Convey("get volume", func() {
-			gainedVolume, err := client.GetVolume(resourceTestVolume, nil)
-			So(err, ShouldBeNil)
-			So(gainedVolume, ShouldResemble, referenceVolume)
+	client := newResourceClient(test)
+	Convey("Test volume methods", test, func() {
+		Convey("resource api", func() {
+			referenceVolumes := newFakeResourceVolume(test)
+			Convey("get volume", func() {
+				gainedVolume, err := client.GetVolume(referenceVolumes[0].Label, nil)
+				So(err, ShouldBeNil)
+				So(gainedVolume, ShouldResemble, referenceVolumes[0])
+			})
+			Convey("get volume list", func() {
+				gainedList, err := client.GetVolumeList(nil, nil)
+				So(err, ShouldBeNil)
+				So(gainedList, ShouldResemble, referenceVolumes)
+			})
 		})
 	})
 }
