20be2dd76ad1383ffbe54ea0a701179a7c0e9195
paul <petrukhin.paul@gmail.com>
2018-02-14 13:37:50 +0300

    add status code

--- pkg/model/errors.go
+++ pkg/model/errors.go
@@ -1,9 +1,14 @@
 package model
 
+import (
+	"fmt"
+)
+
 type ResourceError struct {
-	ErrorMsg string `json:"error"`
+	StatusCode int    `json:"status_code"`
+	ErrorMsg   string `json:"error"`
 }
 
 func (err *ResourceError) Error() string {
-	return err.ErrorMsg
+	return fmt.Sprintf("status %d: %s ", err.StatusCode, err.ErrorMsg)
 }
