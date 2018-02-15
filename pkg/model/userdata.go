6eacd677222df5b6164085d163feaffc556c8929
paul <petrukhin.paul@gmail.com>
2018-02-15 15:10:34 +0300

    add user model

--- pkg/model/userdata.go
+++ pkg/model/userdata.go
@@ -5,3 +5,22 @@ type UserHeaderData struct {
 	Label  string `json:"label"`  // user-visible label for the object
 	Access string `json:"access"` // one of: "owner", "read", "write", "read-delete", "none"
 }
+
+type User struct {
+	Login     string   `json:"login"`
+	Data      UserData `json:"data"`
+	ID        string   `json:"id"`
+	IsActive  bool     `json:"is_active"`
+	CreatedAt string   `json:"created_at"`
+}
+
+type UserData struct {
+	Email          string `json:"email"`
+	Address        string `json:"address"`
+	Phone          string `json:"phone"`
+	FirstName      string `json:"first_name"`
+	LastName       string `json:"last_name"`
+	IsOrganization bool   `json:"is_organization"`
+	TaxCode        string `json:"tax_code"`
+	Company        string `json:"company"`
+}
