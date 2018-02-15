0b9894f885acbc63697d2f317741d486d27dd0f4
Ilia Donchenko <iliad@me.com>
2018-02-15 14:58:35 +0300

    Remove endpoint model

--- pkg/model/endpoint.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Endpoint struct {
-	Name      string   `json:"name" binding:"required"`
-	Owner     *string  `json:"owner,omitempty" binding:"required"`
-	CreatedAt *int64   `json:"created_at,omitempty"`
-	Addresses []string `json:"addresses" binding:"required"`
-	Ports     []Port   `json:"ports" binding:"required"`
-}
