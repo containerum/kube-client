3fcba7f55624ee516ce6544831dafa9e165640e8
paul <petrukhin.paul@gmail.com>
2018-02-15 09:20:57 +0300

    remove err parameter

--- pkg/cmd/deployment.go
+++ pkg/cmd/deployment.go
@@ -10,22 +10,23 @@ const (
 	kubeAPIdeploymentPath  = "/namespaces/{namespace}/deployments/{deployment}"
 	kubeAPIdeploymentsPath = "/namespaces/{namespace}/deployments"
 
-	resourceDeploymentPath = "/namespace/{namespace}/deployment/{deployment}"
-	resourceImagePath      = resourceDeploymentPath + "/image"
-	resourceReplicasPath   = resourceDeploymentPath + "/replicas"
+	resourceDeploymentRootPath = "/namespace/{namespace}/deployment"
+	resourceDeploymentPath     = resourceDeploymentRootPath + "/{deployment}"
+	resourceImagePath          = resourceDeploymentPath + "/image"
+	resourceReplicasPath       = "/namespace/{namespace}/deployment/{deployment}/replicas"
 )
 
 // GetDeployment -- consumes a namespace and a deployment names,
 // returns a Deployment data OR uninitialized struct AND an error
 func (client *Client) GetDeployment(namespace, deployment string) (model.Deployment, error) {
-	resp, err := client.Request.
+	resp, _ := client.Request.
 		SetPathParams(map[string]string{
 			"namespace":  namespace,
 			"deployment": deployment,
 		}).SetResult(model.Deployment{}).
 		SetError(model.ResourceError{}).
 		Get(client.serverURL + kubeAPIdeploymentPath)
-	if err := catchErr(err, resp, http.StatusOK); err != nil {
+	if err := catchErr(resp, http.StatusOK); err != nil {
 		return model.Deployment{}, err
 	}
 	return *resp.Result().(*model.Deployment), nil
@@ -34,13 +35,13 @@ func (client *Client) GetDeployment(namespace, deployment string) (model.Deploym
 // GetDeploymentList -- consumes a namespace and a deployment names,
 // returns a list of Deployments OR nil slice AND an error
 func (client *Client) GetDeploymentList(namespace string) ([]model.Deployment, error) {
-	resp, err := client.Request.
+	resp, _ := client.Request.
 		SetPathParams(map[string]string{
 			"namespace": namespace,
 		}).SetResult([]model.Deployment{}).
 		SetError(model.ResourceError{}).
 		Get(client.serverURL + kubeAPIdeploymentsPath)
-	if err := catchErr(err, resp, http.StatusOK); err != nil {
+	if err := catchErr(resp, http.StatusOK); err != nil {
 		return nil, err
 	}
 	return *resp.Result().(*[]model.Deployment), nil
@@ -49,65 +50,63 @@ func (client *Client) GetDeploymentList(namespace string) ([]model.Deployment, e
 // DeleteDeployment -- consumes a namespace, a deployment,
 // an user role and an ID
 func (client *Client) DeleteDeployment(namespace, deployment string) error {
-	resp, err := client.Request.
+	resp, _ := client.Request.
 		SetPathParams(map[string]string{
 			"namespace":  namespace,
 			"deployment": deployment,
 		}).SetError(model.ResourceError{}).
 		Delete(client.resourceServiceAddr + resourceDeploymentPath)
-	return catchErr(err, resp,
-		http.StatusOK,
-		http.StatusNoContent)
+	return catchErr(resp, http.StatusOK)
 }
 
 // CreateDeployment -- consumes a namespace, an user ID and a Role,
 // returns nil if OK
 func (client *Client) CreateDeployment(namespace string, deployment model.Deployment) error {
-	resp, err := client.Request.
+	resp, _ := client.Request.
 		SetPathParams(map[string]string{
 			"namespace": namespace,
 		}).SetBody(deployment).
 		SetError(model.ResourceError{}).
-		Post(client.resourceServiceAddr + "/namespace/{namespace}/deployment")
-	return catchErr(err, resp,
+		Post(client.resourceServiceAddr + resourceDeploymentRootPath)
+	return catchErr(resp,
+		http.StatusOK,
 		http.StatusCreated,
 		http.StatusAccepted)
 }
 
 func (client *Client) SetContainerImage(namespace, deployment string, updateImage model.UpdateImage) error {
-	resp, err := client.Request.
+	resp, _ := client.Request.
 		SetPathParams(map[string]string{
 			"namespace":  namespace,
 			"deployment": deployment,
 		}).SetBody(updateImage).
 		Put(client.resourceServiceAddr + resourceImagePath)
-	return catchErr(err, resp,
+	return catchErr(resp,
 		http.StatusAccepted,
 		http.StatusOK,
 		http.StatusNoContent)
 }
 
 func (client *Client) ReplaceDeployment(namespace string, deployment model.Deployment) error {
-	resp, err := client.Request.
+	resp, _ := client.Request.
 		SetPathParams(map[string]string{
 			"namespace":  namespace,
 			"deployment": deployment.Name,
 		}).SetBody(deployment).
 		Put(client.resourceServiceAddr + resourceDeploymentPath)
-	return catchErr(err, resp,
+	return catchErr(resp,
 		http.StatusAccepted,
 		http.StatusOK,
 		http.StatusNoContent)
 }
 
 func (client *Client) SetReplicas(namespace, deployment string, replicas int) error {
-	resp, err := client.Request.SetPathParams(map[string]string{
+	resp, _ := client.Request.SetPathParams(map[string]string{
 		"namespace":  namespace,
 		"deployment": deployment,
 	}).SetBody(model.UpdateReplicas{replicas}).
 		Put(client.resourceServiceAddr + resourceReplicasPath)
-	return catchErr(err, resp,
+	return catchErr(resp,
 		http.StatusAccepted,
-		http.StatusOK,
-		http.StatusNoContent)
+		http.StatusOK)
 }
