4519e0149fe1a894bb17aa74314ba4fafadb9ce7
paul <petrukhin.paul@gmail.com>
2018-02-15 09:55:15 +0300

    fix namespace methods

--- pkg/cmd/namespace.go
+++ pkg/cmd/namespace.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"net/http"
 	"strconv"
 
 	"git.containerum.net/ch/kube-client/pkg/model"
@@ -24,7 +25,7 @@ func (client *Client) GetNamespaceList(queries map[string]string) ([]model.Names
 		SetQueryParams(queries).
 		SetResult([]model.Namespace{}).
 		Get(client.serverURL + getNamespaceList)
-	if err != nil {
+	if err := catchErr(err, resp, http.StatusOK); err != nil {
 		return []model.Namespace{}, err
 	}
 	return *resp.Result().(*[]model.Namespace), nil
@@ -37,7 +38,7 @@ func (client *Client) GetNamespace(ns string) (model.Namespace, error) {
 			"namespace": ns,
 		}).
 		Get(client.serverURL + getNamespace)
-	if err != nil {
+	if err := catchErr(err, resp, http.StatusOK); err != nil {
 		return model.Namespace{}, err
 	}
 	return *resp.Result().(*model.Namespace), nil
@@ -45,16 +46,17 @@ func (client *Client) GetNamespace(ns string) (model.Namespace, error) {
 
 // ResourceGetNamespace -- consumes a namespace and an optional user ID
 // returns a namespace data OR an error
-func (client *Client) ResourceGetNamespace(namespace, userID string) (model.ResourceNamespace, error) {
+func (client *Client) ResourceGetNamespace(namespace string, userID *string) (model.ResourceNamespace, error) {
 	req := client.Request.
 		SetPathParams(map[string]string{
 			"namespace": namespace,
-		}).SetResult(model.ResourceNamespace{})
-	if userID != "" {
-		req.SetQueryParam("user-id", userID)
+		}).SetResult(model.ResourceNamespace{}).
+		SetResult(model.ResourceError{})
+	if userID != nil {
+		req.SetQueryParam("user-id", *userID)
 	}
 	resp, err := req.Get(client.resourceServiceAddr + serviceNamespacePath)
-	if err != nil {
+	if err := catchErr(err, resp, http.StatusOK); err != nil {
 		return model.ResourceNamespace{}, nil
 	}
 	return *resp.Result().(*model.ResourceNamespace), nil
@@ -68,12 +70,13 @@ func (client *Client) ResourceGetNamespaceList(page, perPage uint64, userID stri
 		SetQueryParams(map[string]string{
 			"page":     strconv.FormatUint(page, 10),
 			"per_page": strconv.FormatUint(perPage, 10),
-		}).SetResult([]model.ResourceNamespace{})
+		}).SetResult([]model.ResourceNamespace{}).
+		SetError(model.ResourceError{})
 	if userID != "" {
 		req.SetQueryParam("user-id", userID)
 	}
 	resp, err := req.Get(client.resourceServiceAddr + serviceNamespacesPath)
-	if err != nil {
+	if err := catchErr(err, resp, http.StatusOK); err != nil {
 		return nil, err
 	}
 	return *resp.Result().(*[]model.ResourceNamespace), nil
