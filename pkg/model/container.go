da80f7f61c8b5f371146d5c6a41e1b52d6c8d810
Ilia Donchenko <iliad@me.com>
2018-02-16 12:18:18 +0300

    Updated binding

--- pkg/model/container.go
+++ pkg/model/container.go
@@ -2,11 +2,11 @@ package model
 
 type Container struct {
 	Name    string    `json:"name" binding:"required"`
-	Env     *[]Env    `json:"env,omitempty" binding:"dive"`
+	Env     *[]Env    `json:"env,omitempty" binding:"omitempty,dive"`
 	Image   string    `json:"image" binding:"required"`
-	Volume  *[]Volume `json:"volume,omitempty" binding:"dive"`
+	Volume  *[]Volume `json:"volume,omitempty" binding:"omitempty,dive"`
 	Limits  Limits    `json:"limits" binding:"required"`
-	Ports   *[]Port   `json:"ports,omitempty" binding:"dive"`
+	Ports   *[]Port   `json:"ports,omitempty" binding:"omitempty,dive"`
 	Command *[]string `json:"command,omitempty"`
 }
 
