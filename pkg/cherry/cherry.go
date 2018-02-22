1c725cc6859bc5989b72b23c4d560846c938bf53
paul <petrukhin.paul@gmail.com>
2018-02-22 11:25:38 +0300

    add docs

--- pkg/cherry/cherry.go
+++ pkg/cherry/cherry.go
@@ -6,17 +6,24 @@ import (
 	"strconv"
 )
 
-const (
-	HeadDelimiter    = ": "
-	DetailsDelimiter = "; "
-)
-
+// Err -- standart serializable API error
+// Message -- constant error message:
+//		+ "invalid username"
+//		+ "quota exceeded"
+//		+ "validation error"
+//		...etc...
+// ID -- unique error identification code
+// Details -- optional context error messages kinda
+// 		+ "field 'Replicas' must be non-zero value"
+//		+ "not enough tights to feed gopher"
+//		+ "resource 'God' does't exist"
 type Err struct {
 	Message string   `json:"message"`
 	ID      uint64   `json:"id"`
-	Details []string `json:"details"`
+	Details []string `json:"details,omitempty"`
 }
 
+// NewErr -- constructs Err struct with provided message and ID
 func NewErr(msg string, ID uint64) *Err {
 	return &Err{
 		Message: msg,
@@ -24,25 +31,37 @@ func NewErr(msg string, ID uint64) *Err {
 	}
 }
 
+// Returns text representation kinda
+// "unable to parse quota []"
 func (err *Err) Error() string {
 	buf := bytes.NewBufferString(err.Message +
-		strconv.FormatUint(err.ID, 10) +
-		HeadDelimiter)
-	for _, msg := range err.Details {
-		buf.WriteString(DetailsDelimiter + msg)
+		" [" + strconv.FormatUint(err.ID, 10) + "]")
+	detailsLen := len(err.Details)
+	if detailsLen > 0 {
+		buf.WriteString(": ")
+	}
+	for i, msg := range err.Details {
+		if i+1 == detailsLen {
+			buf.WriteString(msg)
+		} else {
+			buf.WriteString(msg + "; ")
+		}
 	}
 	return buf.String()
 }
 
+// AddDetails -- adds detail messages to Err, chainable
 func (err *Err) AddDetails(details ...string) *Err {
 	err.Details = append(err.Details, details...)
 	return err
 }
 
+// AddDetailF --adds formatted message to Err, chainable
 func (err *Err) AddDetailF(formatS string, args ...interface{}) *Err {
 	return err.AddDetails(fmt.Sprintf(formatS, args...))
 }
 
+// AddDetailsErr -- adds errors as detail messages to Err, chainable
 func (err *Err) AddDetailsErr(details ...error) *Err {
 	for _, detail := range details {
 		err.AddDetails(detail.Error())
