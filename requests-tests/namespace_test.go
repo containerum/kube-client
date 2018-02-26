adfcbe4b3c03f0ff820fbbe1bb4329914cb3fa51
paul <petrukhin.paul@gmail.com>
2018-02-26 10:54:39 +0300

    add volume tests

--- requests-tests/namespace_test.go
+++ requests-tests/namespace_test.go
@@ -21,12 +21,12 @@ func TestNamespace(test *testing.T) {
 	})
 	Convey("Test resource service methods", test, func() {
 		Convey("get namespace", func() {
-			gainedNamespace, err := client.ResourceGetNamespace(fakeNamespaces[0].Label, nil)
+			gainedNamespace, err := client.ResourceGetNamespace(fakeNamespaces[0].Label)
 			So(err, ShouldBeNil)
 			So(gainedNamespace, ShouldResemble, fakeNamespaces[0])
 		})
 		Convey("get namespace list", func() {
-			_, err := client.ResourceGetNamespaceList(0, 16, "")
+			_, err := client.ResourceGetNamespaceList(0, 16)
 			So(err, ShouldBeNil)
 		})
 		Convey("rename namespace", func() {
