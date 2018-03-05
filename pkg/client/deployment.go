b12854f07ca92f3e8edb3da3d62cfa9909ad3d33
paul <petrukhin.paul@gmail.com>
2018-03-05 14:35:13 +0300

    refactor deployment methods

--- pkg/client/deployment.go
+++ pkg/client/deployment.go
@@ -1,9 +1,8 @@
 package client
 
 import (
-	"git.containerum.net/ch/kube-client/pkg/rest"
-
 	"git.containerum.net/ch/kube-client/pkg/model"
+	. "git.containerum.net/ch/kube-client/pkg/rest"
 )
 
 const (
@@ -20,10 +19,16 @@ const (
 // returns a Deployment data OR uninitialized struct AND an error
 func (client *Client) GetDeployment(namespace, deployment string) (model.Deployment, error) {
 	var depl model.Deployment
-	err := client.re.Get(&depl, rest.P{
-		"namespace":  namespace,
-		"deployment": deployment,
-	}, client.APIurl, kubeAPIdeploymentPath)
+	err := client.re.Get(Rq{
+		Result: &depl,
+		Path: URL{
+			Templ: client.APIurl + kubeAPIdeploymentPath,
+			Params: P{
+				"namespace":  namespace,
+				"deployment": deployment,
+			},
+		},
+	})
 	return depl, err
 }
 
@@ -31,52 +36,87 @@ func (client *Client) GetDeployment(namespace, deployment string) (model.Deploym
 // returns a list of Deployments OR nil slice AND an error
 func (client *Client) GetDeploymentList(namespace string) ([]model.Deployment, error) {
 	var depls []model.Deployment
-	err := client.re.Get(&depls, rest.P{
-		"namespace": namespace,
-	}, client.APIurl, kubeAPIdeploymentsPath)
+	err := client.re.Get(Rq{
+		Result: &depls,
+		Path: URL{
+			Templ: client.APIurl + kubeAPIdeploymentsPath,
+			Params: P{
+				"namespace": namespace,
+			},
+		},
+	})
 	return depls, err
 }
 
 // DeleteDeployment -- consumes a namespace, a deployment,
 // an user role and an ID
 func (client *Client) DeleteDeployment(namespace, deployment string) error {
-	return client.re.Delete(rest.P{
-		"namespace":  namespace,
-		"deployment": deployment,
-	}, client.APIurl, resourceDeploymentPath)
+	return client.re.Delete(Rq{
+		Path: URL{
+			Templ: client.APIurl + resourceDeploymentPath,
+			Params: P{
+				"namespace":  namespace,
+				"deployment": deployment,
+			},
+		},
+	})
 }
 
 // CreateDeployment -- consumes a namespace, an user ID and a Role,
 // returns nil if OK
 func (client *Client) CreateDeployment(namespace string, deployment model.Deployment) error {
-	return client.re.Post(nil, deployment, rest.P{
-		"namespace": namespace,
-	}, client.APIurl, resourceDeploymentRootPath)
+	return client.re.Post(Rq{
+		Body: deployment,
+		Path: URL{
+			Templ: client.APIurl + resourceDeploymentRootPath,
+			Params: P{
+				"namespace": namespace,
+			},
+		},
+	})
 }
 
 // SetContainerImage -- set or changes deployment container image
 // Consumes namespace, deployment and container data
 func (client *Client) SetContainerImage(namespace, deployment string, updateImage model.UpdateImage) error {
-	return client.re.Put(nil, updateImage, rest.P{
-		"namespace":  namespace,
-		"deployment": deployment,
-	}, client.APIurl, resourceImagePath)
+	return client.re.Put(Rq{
+		Body: updateImage,
+		Path: URL{
+			Templ: client.APIurl + resourceImagePath,
+			Params: P{
+				"namespace":  namespace,
+				"deployment": deployment,
+			},
+		},
+	})
 }
 
 // ReplaceDeployment -- replaces deployment in provided namespace with new one
 func (client *Client) ReplaceDeployment(namespace string, deployment model.Deployment) error {
-	return client.re.Put(nil, deployment, rest.P{
-		"namespace":  namespace,
-		"deployment": deployment.Name,
-	}, client.APIurl, resourceDeploymentPath)
+	return client.re.Put(Rq{
+		Body: deployment,
+		Path: URL{
+			Templ: client.APIurl + resourceDeploymentPath,
+			Params: P{
+				"namespace":  namespace,
+				"deployment": deployment.Name,
+			},
+		},
+	})
 }
 
 // SetReplicas -- sets or changes deployment replicas
 func (client *Client) SetReplicas(namespace, deployment string, replicas int) error {
-	return client.re.Put(nil, model.UpdateReplicas{
-		Replicas: replicas,
-	}, rest.P{
-		"namespace":  namespace,
-		"deployment": deployment,
-	}, client.APIurl, resourceReplicasPath)
+	return client.re.Put(Rq{
+		Body: model.UpdateReplicas{
+			Replicas: replicas,
+		},
+		Path: URL{
+			Templ: client.APIurl + resourceReplicasPath,
+			Params: P{
+				"namespace":  namespace,
+				"deployment": deployment,
+			},
+		},
+	})
 }
