d3846bf3cee2010aff9796f97ac798222d6ebfcc
paul <petrukhin.paul@gmail.com>
2018-03-05 12:26:27 +0300

    add docs

--- pkg/rest/interface.go
+++ pkg/rest/interface.go
@@ -1,7 +1,9 @@
 package rest
 
+// P -- URL path params
 type P map[string]string
 
+// REST -- rest client interface
 type REST interface {
 	Get(interface{}, P, ...string) error
 	Put(interface{}, P, ...string) error
