d46db53a321354824a4570d0d9931998f5e5019e
paul <petrukhin.paul@gmail.com>
2018-03-05 12:47:56 +0300

    add result params

--- pkg/rest/interface.go
+++ pkg/rest/interface.go
@@ -5,8 +5,8 @@ type P map[string]string
 
 // REST -- rest client interface
 type REST interface {
-	Get(interface{}, P, ...string) error
-	Put(interface{}, P, ...string) error
-	Post(interface{}, P, ...string) error
-	Delete(P, ...string) error
+	Get(result interface{}, params P, path ...string) error
+	Put(result, body interface{}, params P, path ...string) error
+	Post(result, body interface{}, params P, path ...string) error
+	Delete(params P, path ...string) error
 }
