56598e98a4f2216701017cab61e8d21f5268b386
paul <petrukhin.paul@gmail.com>
2018-03-05 13:09:54 +0300

    update deployment methods

--- pkg/client/deployment.go
+++ pkg/client/deployment.go
@@ -1,9 +1,8 @@
 package client
 
 import (
-	"net/http"
+	"git.containerum.net/ch/kube-client/pkg/rest"
 
-	"git.containerum.net/ch/kube-client/pkg/cherry"
 	"git.containerum.net/ch/kube-client/pkg/model"
 )
 
@@ -20,99 +19,64 @@ const (
 // GetDeployment -- consumes a namespace and a deployment names,
 // returns a Deployment data OR uninitialized struct AND an error
 func (client *Client) GetDeployment(namespace, deployment string) (model.Deployment, error) {
-	resp, err := client.Request.
-		SetPathParams(map[string]string{
-			"namespace":  namespace,
-			"deployment": deployment,
-		}).SetResult(model.Deployment{}).
-		SetError(cherry.Err{}).
-		Get(client.APIurl + kubeAPIdeploymentPath)
-	if err := MapErrors(resp, err, http.StatusOK); err != nil {
-		return model.Deployment{}, err
-	}
-	return *resp.Result().(*model.Deployment), nil
+	var depl model.Deployment
+	err := client.re.Get(&depl, rest.P{
+		"namespace":  namespace,
+		"deployment": deployment,
+	}, client.APIurl, kubeAPIdeploymentPath)
+	return depl, err
 }
 
 // GetDeploymentList -- consumes a namespace and a deployment names,
 // returns a list of Deployments OR nil slice AND an error
 func (client *Client) GetDeploymentList(namespace string) ([]model.Deployment, error) {
-	resp, err := client.Request.
-		SetPathParams(map[string]string{
-			"namespace": namespace,
-		}).SetResult([]model.Deployment{}).
-		SetError(cherry.Err{}).
-		Get(client.APIurl + kubeAPIdeploymentsPath)
-	if err := MapErrors(resp, err, http.StatusOK); err != nil {
-		return nil, err
-	}
-	return *resp.Result().(*[]model.Deployment), nil
+	var depls []model.Deployment
+	err := client.re.Get(&depls, rest.P{
+		"namespace": namespace,
+	}, client.APIurl, kubeAPIdeploymentsPath)
+	return depls, err
 }
 
 // DeleteDeployment -- consumes a namespace, a deployment,
 // an user role and an ID
 func (client *Client) DeleteDeployment(namespace, deployment string) error {
-	resp, err := client.Request.
-		SetPathParams(map[string]string{
-			"namespace":  namespace,
-			"deployment": deployment,
-		}).SetError(cherry.Err{}).
-		Delete(client.ResourceAddr + resourceDeploymentPath)
-	return MapErrors(resp, err, http.StatusOK)
+	return client.re.Delete(rest.P{
+		"namespace":  namespace,
+		"deployment": deployment,
+	}, client.APIurl, resourceDeploymentPath)
 }
 
 // CreateDeployment -- consumes a namespace, an user ID and a Role,
 // returns nil if OK
 func (client *Client) CreateDeployment(namespace string, deployment model.Deployment) error {
-	resp, err := client.Request.
-		SetPathParams(map[string]string{
-			"namespace": namespace,
-		}).SetBody(deployment).
-		SetError(cherry.Err{}).
-		Post(client.ResourceAddr + resourceDeploymentRootPath)
-	return MapErrors(resp, err,
-		http.StatusOK,
-		http.StatusCreated,
-		http.StatusAccepted)
+	return client.re.Post(nil, deployment, rest.P{
+		"namespace": namespace,
+	}, client.APIurl, resourceDeploymentRootPath)
 }
 
 // SetContainerImage -- set or changes deployment container image
 // Consumes namespace, deployment and container data
 func (client *Client) SetContainerImage(namespace, deployment string, updateImage model.UpdateImage) error {
-	resp, err := client.Request.
-		SetPathParams(map[string]string{
-			"namespace":  namespace,
-			"deployment": deployment,
-		}).SetBody(updateImage).
-		SetError(cherry.Err{}).
-		Put(client.ResourceAddr + resourceImagePath)
-	return MapErrors(resp, err,
-		http.StatusAccepted,
-		http.StatusOK,
-		http.StatusNoContent)
+	return client.re.Put(nil, updateImage, rest.P{
+		"namespace":  namespace,
+		"deployment": deployment,
+	}, client.APIurl, resourceImagePath)
 }
 
 // ReplaceDeployment -- replaces deployment in provided namespace with new one
 func (client *Client) ReplaceDeployment(namespace string, deployment model.Deployment) error {
-	resp, err := client.Request.
-		SetPathParams(map[string]string{
-			"namespace":  namespace,
-			"deployment": deployment.Name,
-		}).SetBody(deployment).
-		SetError(cherry.Err{}).
-		Put(client.ResourceAddr + resourceDeploymentPath)
-	return MapErrors(resp, err, http.StatusOK)
+	return client.re.Put(nil, deployment, rest.P{
+		"namespace":  namespace,
+		"deployment": deployment.Name,
+	}, client.APIurl, resourceDeploymentPath)
 }
 
 // SetReplicas -- sets or changes deployment replicas
 func (client *Client) SetReplicas(namespace, deployment string, replicas int) error {
-	resp, err := client.Request.SetPathParams(map[string]string{
+	return client.re.Put(nil, model.UpdateReplicas{
+		Replicas: replicas,
+	}, rest.P{
 		"namespace":  namespace,
 		"deployment": deployment,
-	}).SetBody(model.UpdateReplicas{
-		Replicas: replicas,
-	}).SetError(cherry.Err{}).
-		Put(client.ResourceAddr + resourceReplicasPath)
-	return MapErrors(resp, err,
-		http.StatusAccepted,
-		http.StatusOK)
+	}, client.APIurl, resourceReplicasPath)
 }
