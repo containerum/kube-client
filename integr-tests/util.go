e3ccffd0079f05ed14cddbbfcc86a913a343cc90
paul <petrukhin.paul@gmail.com>
2018-03-06 13:37:14 +0300

    rm mock constructor

--- integr-tests/util.go
+++ integr-tests/util.go
@@ -5,27 +5,13 @@ import (
 
 	kubeClient "git.containerum.net/ch/kube-client/pkg/client"
 	"git.containerum.net/ch/kube-client/pkg/rest/re"
-	"git.containerum.net/ch/kube-client/pkg/rest/remock"
 )
 
-func newMockClient(test *testing.T) *kubeClient.Client {
-	client, err := kubeClient.NewClient(kubeClient.Config{
-		APIurl:  "http://192.168.88.200",
-		RestAPI: remock.NewMock(),
-		User: kubeClient.User{
-			Role: "user",
-		},
-	})
-	if err != nil {
-		test.Fatalf("error while client initialisation: %v", err)
-	}
-	return client
-}
 func newClient(test *testing.T) *kubeClient.Client {
 	client, err := kubeClient.NewClient(
 		kubeClient.Config{
 			RestAPI: re.NewResty(re.SkipTLSVerify),
-			APIurl:  "https://192.168.88.200",
+			APIurl:  "https://192.168.88.200:8082",
 			User: kubeClient.User{
 				Role: "user",
 			},
