48e03be512b952af55b11a4455d9c6ee5b532e96
paul <petrukhin.paul@gmail.com>
2018-03-12 16:25:08 +0300

    add details method

--- pkg/cherry/constructor.go
+++ pkg/cherry/constructor.go
@@ -5,3 +5,33 @@ type ErrConstruct func(...func(*Err)) *Err
 func (constr ErrConstruct) Error() string {
 	return constr().Error()
 }
+
+func (constr ErrConstruct) AddDetails(details ...string) ErrConstruct {
+	err := constr().AddDetails(details...)
+	return func(options ...func(*Err)) *Err {
+		for _, option := range options {
+			option(err)
+		}
+		return err
+	}
+}
+
+func (constr ErrConstruct) AddDetailsErr(details ...error) ErrConstruct {
+	err := constr().AddDetailsErr(details...)
+	return func(options ...func(*Err)) *Err {
+		for _, option := range options {
+			option(err)
+		}
+		return err
+	}
+}
+
+func (constr ErrConstruct) AddDetailF(f string, vals ...interface{}) ErrConstruct {
+	err := constr().AddDetailF(f, vals...)
+	return func(options ...func(*Err)) *Err {
+		for _, option := range options {
+			option(err)
+		}
+		return err
+	}
+}
