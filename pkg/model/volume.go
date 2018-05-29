56ecbe26dee61b8267b58e8956f48e2bd610734f
Ilia Donchenko <iliad@me.com>
2018-05-29 16:05:51 +0300

    Fix access

--- pkg/model/volume.go
+++ pkg/model/volume.go
@@ -9,7 +9,7 @@ type Volume struct {
 	Owner       string                     `json:"owner,omitempty"`
 	OwnerLogin  string                     `json:"owner_login,omitempty"`
 	Label       string                     `json:"label,omitempty"`
-	Access      string                     `json:"access,omitempty"`
+	Access      AccessLevel                `json:"access,omitempty"`
 	Capacity    uint                       `json:"capacity,omitempty"`
 	StorageName string                     `json:"storage_name,omitempty"` //AKA StorageClass
 	AccessMode  PersistentVolumeAccessMode `json:"access_mode,omitempty"`
