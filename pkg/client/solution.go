f686b1f47d2a9436f70cc4682cb5e1a2223afb58
Ilia Donchenko <iliad@me.com>
2018-06-15 11:36:40 +0300

    Solutions routes

--- pkg/client/solution.go
+++ pkg/client/solution.go
@@ -6,13 +6,67 @@ import (
 )
 
 const (
-	solutionsPath     = "/solutions"
-	userSolutionsPath = "/user_solutions"
+	templatesPath   = "/templates"
+	templateEnvPath = "/templates/{template}/env"
+	templateResPath = "/templates/{template}/resources"
+	solutionsPath   = "/solutions"
+	solutionPath    = "/solutions/{solution}"
 )
 
-// GetSolutionList -- returns list of public solutions
-func (client *Client) GetSolutionList() (model.AvailableSolutionsList, error) {
+// GetSolutionList -- returns list of public solutions templates
+func (client *Client) GetSolutionsTemplatesList() (model.AvailableSolutionsList, error) {
 	var solutionList model.AvailableSolutionsList
+	err := client.RestAPI.Get(rest.Rq{
+		Result: &solutionList,
+		URL: rest.URL{
+			Path: templatesPath,
+		},
+	})
+	return solutionList, err
+}
+
+func (client *Client) GetSolutionsTemplateEnv(templateName string) (model.SolutionEnv, error) {
+	var solutionEnv model.SolutionEnv
+	err := client.RestAPI.Get(rest.Rq{
+		Result: &solutionEnv,
+		URL: rest.URL{
+			Path: templateEnvPath,
+			Params: rest.P{
+				"template": templateName,
+			},
+		},
+	})
+	return solutionEnv, err
+}
+
+func (client *Client) GetSolutionsTemplateResources(templateName string) (model.SolutionResources, error) {
+	var solutionRes model.SolutionResources
+	err := client.RestAPI.Get(rest.Rq{
+		Result: &solutionRes,
+		URL: rest.URL{
+			Path: templateResPath,
+			Params: rest.P{
+				"template": templateName,
+			},
+		},
+	})
+	return solutionRes, err
+}
+
+func (client *Client) RunSolution(solution model.UserSolution) (model.RunSolutionResponse, error) {
+	var resp model.RunSolutionResponse
+	err := client.RestAPI.Post(rest.Rq{
+		Result: &resp,
+		Body:   solution.Copy(),
+		URL: rest.URL{
+			Path: solutionsPath,
+		},
+	})
+	return resp, err
+}
+
+func (client *Client) GetSolutionsList() (model.UserSolutionsList, error) {
+	var solutionList model.UserSolutionsList
 	err := client.RestAPI.Get(rest.Rq{
 		Result: &solutionList,
 		URL: rest.URL{
@@ -22,14 +76,16 @@ func (client *Client) GetSolutionList() (model.AvailableSolutionsList, error) {
 	return solutionList, err
 }
 
-func (client *Client) RunSolution(solution model.UserSolution) (model.RunSolutionResponse, error) {
-	var resp model.RunSolutionResponse
-	err := client.RestAPI.Post(rest.Rq{
-		Result: &resp,
-		Body:   solution.Copy(),
+func (client *Client) GetSolution(solutionName string) (model.UserSolutionsList, error) {
+	var solutionList model.UserSolutionsList
+	err := client.RestAPI.Get(rest.Rq{
+		Result: &solutionList,
 		URL: rest.URL{
-			Path: userSolutionsPath,
+			Path: solutionPath,
+			Params: rest.P{
+				"solution": solutionName,
+			},
 		},
 	})
-	return resp, err
+	return solutionList, err
 }
